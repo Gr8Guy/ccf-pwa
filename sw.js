1c10d8a87238e9d0c5131d45aec061489b1c7706
Guy Gilbert <guysmailbox@yahoo.com>
2022-07-18 13:15:55 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
 var GHPATH = '/ccf-pwa';
 var APP_PREFIX = 'ccf_';
-var VERSION = 'version_003';
+var VERSION = 'version_004';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/css/styles.css`,
   `${GHPATH}/img/icon.png`,
   `${GHPATH}/js/app.js`
+  `${GHPATH}/js/jquery-3.1.0.min.js`
 ]
 
 var CACHE_NAME = APP_PREFIX + VERSION
